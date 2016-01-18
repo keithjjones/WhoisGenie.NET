76f08f58a6a202265613302f9ba6e677692511f3
Keith J. Jones <keith@keithjjones.com>
2016-01-18 10:47:18 -0500

    Added ARINResponse.

---
+++ WhoisGenie.NET/ARINResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArinWhois;
+
+namespace WhoisGenie.NET
+{
+    public class ARINResponse : ArinWhois.Model.Response
+    {
+        // Forward this model to the user of this library.s
+    }
+}
